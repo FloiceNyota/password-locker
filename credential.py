a67f6472333d5e46a86ff5f1953a9745913f25d3
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-19 04:02:09 +0300

    create class to generate new credential for users

---
+++ credential.py
@@ -0,0 +1,7 @@
+#Helps with encryption and unseen security structure in backend
+from user import User
+class Credential:
+    """
+    a class that generates new credential for users
+    """
+    
