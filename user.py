f0b2c29ff6e79fe30c4e86fc27d11410d0ea4fd0
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-19 03:42:46 +0300

    create user class

--- user.py
+++ user.py
@@ -1,3 +1,13 @@
-class User:
-    user_list = []
-    
+class User: # generates new instance of user
+    user_list = [] #data structure
+
+    def __init__(self,user_name,password): #constructor method
+        self.user_name = user_name
+        self.password = password
+
+    def save_user(self):
+
+        User.user_list.append(self)    
+
+
+
